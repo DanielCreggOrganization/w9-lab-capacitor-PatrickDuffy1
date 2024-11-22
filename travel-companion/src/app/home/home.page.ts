49d14536ee5d37394041cc8ab3f04b3c4b43d44a
Patrick Duffy <101258287+PatrickDuffy1@users.noreply.github.com>
2024-11-22 10:51:05 +0000

    Completed Part 5. Network Information

--- travel-companion/src/app/home/home.page.ts
+++ travel-companion/src/app/home/home.page.ts
@@ -3,6 +3,7 @@ import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/a
 import { CameraService } from '../services/camera.service';
 import { LocationService } from '../services/location.service';
 import { DeviceInfoService } from '../services/device-info.service';
+import { NetworkInfoService } from '../services/network-info.service';
 //import {defineCustomElements} from '@ionic/pwa-elements/loader'
 
 @Component({
@@ -17,13 +18,15 @@ export class HomePage {
   cameraService : CameraService;
   locationService : LocationService;
   deviceInfoService : DeviceInfoService;
+  networkInfoServive : NetworkInfoService;
   capturedImage: string | undefined;
 
-  constructor(cameraService : CameraService, locationService : LocationService, deviceInfoService : DeviceInfoService) 
+  constructor(cameraService : CameraService, locationService : LocationService, deviceInfoService : DeviceInfoService, networkInfoServive: NetworkInfoService) 
   {
     this.cameraService = cameraService;
     this.locationService = locationService;
     this.deviceInfoService = deviceInfoService;
+    this.networkInfoServive = networkInfoServive;
   }
 
   async takePicture()
@@ -39,5 +42,10 @@ export class HomePage {
   async getDeviceInfo() 
   {
     this.deviceInfoService.getDeviceInfo();
+  }
+
+  async getNetworkInfo() 
+  {
+    this.networkInfoServive.logCurrentNetworkStatus();
   }
 }
---
+++ travel-companion/src/app/services/network-info.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Network } from '@capacitor/network';
+
+Network.addListener('networkStatusChange', status => {
+    console.log('Network status changed', status);
+  });
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class NetworkInfoService {
+
+  logCurrentNetworkStatus = async () => {
+    const status = await Network.getStatus();
+  
+    console.log('Network status:', status);
+  };
+}
