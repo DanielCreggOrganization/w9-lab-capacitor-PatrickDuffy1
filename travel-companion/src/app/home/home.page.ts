e8e8f8557d0541db0b893389c079409d59eb5de5
Patrick Duffy <101258287+PatrickDuffy1@users.noreply.github.com>
2024-11-22 10:41:43 +0000

    Completed Step 4. Device Information

--- travel-companion/src/app/home/home.page.ts
+++ travel-companion/src/app/home/home.page.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
 import { CameraService } from '../services/camera.service';
 import { LocationService } from '../services/location.service';
+import { DeviceInfoService } from '../services/device-info.service';
+//import {defineCustomElements} from '@ionic/pwa-elements/loader'
 
 @Component({
   selector: 'app-home',
@@ -14,20 +16,28 @@ export class HomePage {
 
   cameraService : CameraService;
   locationService : LocationService;
+  deviceInfoService : DeviceInfoService;
+  capturedImage: string | undefined;
 
-  constructor(cameraService : CameraService, locationService : LocationService) 
+  constructor(cameraService : CameraService, locationService : LocationService, deviceInfoService : DeviceInfoService) 
   {
     this.cameraService = cameraService;
     this.locationService = locationService;
+    this.deviceInfoService = deviceInfoService;
   }
 
-  takePicture()
+  async takePicture()
   {
-    this.cameraService.takePicture();
+    this.capturedImage = await this.cameraService.takePicture();
   }
 
   async getCurrentPosition() 
   {
     this.locationService.getCurrentPosition();
+  }
+
+  async getDeviceInfo() 
+  {
+    this.deviceInfoService.getDeviceInfo();
   }
 }
---
+++ travel-companion/src/app/services/device-info.service.ts
@@ -0,0 +1,17 @@
+// src/app/services/device-info.service.ts
+import { Injectable } from '@angular/core';
+import { Device } from '@capacitor/device';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DeviceInfoService {
+  constructor() { }
+
+  async getDeviceInfo() 
+  {
+    const info = await Device.getInfo();
+
+    console.log(info);
+  }
+}
